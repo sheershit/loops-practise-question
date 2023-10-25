21735d91066fcb1028ceecaa06311db397310ede
sheershit <141630396+sheershit@users.noreply.github.com>
2023-10-25 23:34:36 +0530

    Add files via upload

--- main-3.c
+++ main-3.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 int main() {
-    int i,j,k;
+    int i,j,n;
     
-    for(i=1;i<=4;i++)
+    printf("enter the number of lines");
+    scanf("%d",&n);
+    
+    for(i=1;i<=n;i++)
     {
-        k=1;
-        for(j=1;j<=7;j++)
+        for(j=1;j<=2*n-1;j++)
         
-        if(j>=5-i && j<=3+i) {
-        printf("%d",k);
-        j<4? k++ : k--;
-    }
-    else
-    printf(" ");
-    
-    printf("\n");
+        
+        if(j<=5-i || j>=3+i)
+        printf("*");
+        else
+        printf(" ");
+        
+        printf("\n");
+        
     }
     return 0;
 }
